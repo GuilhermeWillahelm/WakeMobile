7602b50bbaf3aed3fd58be17403b7ffdad80f902
GuilhermeWillahelm <guilhermewlm@outlook.com>
2022-07-04 12:29:47 -0300

    Starting project wake version mobile.

---
+++ WakeMobile/WakeMobile/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using WakeMobile.Models;
+using WakeMobile.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WakeMobile.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
